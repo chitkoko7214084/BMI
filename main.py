82f0e013b3aaf2aad10a28ad05d2b19670be35b7
chit ko <80560570+chitkoko7214084@users.noreply.github.com>
2023-06-29 16:10:32 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+
+#author: Chit Ko Ko
+
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+h = float(height)
+w = float(weight)
+h_2 = h ** 2
+
+BMI = int (w/h_2)
+
+
+print (BMI)
